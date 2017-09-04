c65a0de5e01a888e05133d81c2b94196a39ef08f
eudaemonal <eudaemonal@gmail.com>
2017-09-03 22:52:17 -0700

    udpate

--- task1.py
+++ task1.py
@@ -5,6 +5,48 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
+def auto_canny(image, sigma=0.95):
+	# compute the median of the single channel pixel intensities
+	v = np.median(image)
+ 
+	# apply automatic Canny edge detection using the computed median
+	lower = int(max(0, (1.0 - sigma) * v))
+	upper = int(min(255, (1.0 + sigma) * v))
+	edged = cv2.Canny(image, lower, upper)
+ 
+	# return the edged image
+	return edged
+
+
+def angle_cos(p0, p1, p2):
+	d1, d2 = (p0-p1).astype('float'), (p2-p1).astype('float')
+	return abs( np.dot(d1, d2) / np.sqrt( np.dot(d1, d1)*np.dot(d2, d2) ) )
+
+def find_squares(img):
+	img = cv2.GaussianBlur(img, (5, 5), 0)
+	squares = []
+	for gray in cv2.split(img):
+		for thrs in range(0, 255, 26):
+			if thrs == 0:
+				bin = cv2.Canny(gray, 0, 50, apertureSize=5)
+				bin = cv2.dilate(bin, None)
+			else:
+				_retval, bin = cv2.threshold(gray, thrs, 255, cv2.THRESH_BINARY)
+			bin, contours, _hierarchy = cv2.findContours(bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+			for cnt in contours:
+				cnt_len = cv2.arcLength(cnt, True)
+				cnt = cv2.approxPolyDP(cnt, 0.02*cnt_len, True)
+				if len(cnt) == 4 and cv2.contourArea(cnt) > 1000 and cv2.isContourConvex(cnt):
+					cnt = cnt.reshape(-1, 2)
+					max_cos = np.max([angle_cos( cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4] ) for i in range(4)])
+					if max_cos < 0.1:
+						squares.append(cnt)
+	return squares
+
+
+
+
+
 if __name__ == "__main__":
 	video = cv2.VideoCapture('Video_sample.mp4')
 	frameNum = 0;
@@ -27,6 +69,10 @@ if __name__ == "__main__":
 		# Sort them in the order of their distance.
 		matches = sorted(matches, key = lambda x:x.distance)
 		# Draw first 10 matches.
+
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		frame = auto_canny(gray)
+
 		img3 = cv2.drawMatches(model,kp1,frame,kp2, matches[:50],None, flags=2)
 
 		cv2.imshow('frame',img3)
