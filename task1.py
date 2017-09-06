a79ef510d2c58ce27bc1d75a3bcbc52bda4a14fd
eudaemonal <eudaemonal@gmail.com>
2017-09-06 18:52:43 +1000

    udpate

--- task1.py
+++ task1.py
@@ -5,75 +5,81 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-def auto_canny(image, sigma=0.95):
-	# compute the median of the single channel pixel intensities
-	v = np.median(image)
- 
-	# apply automatic Canny edge detection using the computed median
-	lower = int(max(0, (1.0 - sigma) * v))
-	upper = int(min(255, (1.0 + sigma) * v))
-	edged = cv2.Canny(image, lower, upper)
- 
-	# return the edged image
-	return edged
 
 
-def detect_item(img, show_contour, show_center):
+def edgeDetect (img, ksize = 5):
+	sobelX  = cv2.Sobel(img,cv2.CV_16S,1,0,ksize)
+	sobelY  = cv2.Sobel(img,cv2.CV_16S,0,1,ksize)
+	sobel = np.hypot(sobelX, sobelY)/255
+	sobel[sobel > 1] = 1
+
+	return sobel
+
+def findSignificantContours (img, edge):
 	num_item = 0
-	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	gray = cv2.equalizeHist(gray)
-
-	gray = cv2.GaussianBlur(gray,(5,5),0)
-	thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 375, 58)
-
-	edge = auto_canny(thresh)
-
-	(_, cnts, _) = cv2.findContours(edge.copy(), cv2.RETR_EXTERNAL,
+	(_, cnts, _) = cv2.findContours(edge.copy(), cv2.RETR_LIST ,
 			cv2.CHAIN_APPROX_SIMPLE)
-
 	for c in cnts:
 		# compute the center of the contour, then detect the name of the
 		# shape using only the contour
 		M = cv2.moments(c)
 
 		if M['m00'] != 0:
+			
 			cx = int(M['m10']/M['m00'])
 			cy = int(M['m01']/M['m00'])
 			area = cv2.contourArea(c)
 
-			if (24<area) & (area < 40960):
+			# opal: 17900, brush: 24000
+			if (9400<area) & (area < 25000):
 				num_item+=1
-				#print("(%3d, %3d): %3d"%(cx, cy, area))
-				if(show_center):
-					cv2.circle(img,(cx,cy), 1, (0,0,255), 6)
-					location = 'Item #'+str(num_item)+' '+'('+str(cx)+', '+str(cy)+')'
-					cv2.putText(img,location,(cx,cy),cv2.FONT_HERSHEY_SIMPLEX,0.5, (255, 255, 255), 2)
+				# Show centre of item
+				cv2.circle(img,(cx,cy), 1, (0,0,255), 6)
+				location = 'Item #'+str(num_item)+' '+'('+str(cx)+', '+str(cy)+')'
+				cv2.putText(img,location,(cx,cy),cv2.FONT_HERSHEY_SIMPLEX,0.5, (255, 255, 255), 2)
+				# Show contour of item
+				cv2.drawContours(img, c, -1, (0, 255,255), 2, cv2.LINE_AA,maxLevel=1)
 
-				if(show_contour):
-					cv2.drawContours(img, c, -1, (0, 255,255), 3)
+	return img
 
-	return img, num_item
+
+def detect_item(img):
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	gray = cv2.equalizeHist(gray)
+
+	blurred = cv2.GaussianBlur(img, (5, 5), 0) # Remove noise
+	edgeImg = np.max( np.array([ edgeDetect(blurred[:,:, 0]), edgeDetect(blurred[:,:, 1]), edgeDetect(blurred[:,:, 2]) ]), axis=0 )
+	edge= np.array(edgeImg * 255, dtype = np.uint8)
+	mean = np.mean(edge);
+	# Zero any value that is less than mean. This reduces a lot of noise.
+	edge[edge <= mean] = 0
+
+
+	thresh2 = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 31, 18)
+	thresh = cv2.add(thresh2, edge)
+
+	img = findSignificantContours(img, edge)
+
+	return img
 
 
 if __name__ == "__main__":
 	video = cv2.VideoCapture('Video_sample.mp4')
+	ret, model = video.read()
+	h, w, d = model.shape
 
-	frameNum = 0
-	fps = video.get(cv2.CAP_PROP_FPS)
+	frameNum = 1
 
 	while(video.isOpened()):
 		ret, frame = video.read()
 		# Take first frame for matching
-
-		if(frameNum==0):
-			model = frame
 		
 		# Convert RGB to grey scale for processing
 		img1 = cv2.cvtColor(model, cv2.COLOR_BGR2GRAY)
 		img2 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-		frame, num_item = detect_item(frame, True, True)
-		#print("Num: %d"%(num_item))
+		frame = detect_item(frame)
+
 
 		# Initiate ORB detector
 		orb = cv2.ORB_create()
